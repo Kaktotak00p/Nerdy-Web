5acd5ea36791215d94a023b6367dc2e214a94bb5
kaktotak00p <kaktotak00p@gmail.com>
2021-03-02 16:13:32 +0300

    changed home js to set up the leaderboards

--- src/Home.js
+++ src/Home.js
@@ -2,62 +2,67 @@ import React from 'react'
 import { BrowserRouter, Link, Route, Switch, withRouter } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
-const GetLJ = async () => { 
-    var response = await fetch("http://localhost:3001/ljtop")
-    return await response.json()
-}
-    
-const Getbhop = async () => { 
-    var response = await fetch("http://localhost:3001/bhoptop")
-    return await response.json()
-} 
-
-const GetDbop = async () => { 
-    var response = await fetch("http://localhost:3001/dropbhoptop")
-    return await response.json()
-}
-
-const GetLJB = async () => { 
-    var response = await fetch("http://localhost:3001/ljblocktop")
-    return await response.json()
-}
-
-const GetMbhop = async () => { 
-    var response = await fetch("http://localhost:3001/multibhoptop")
-    return await response.json()
-}
-
-const GetWJ = async () => { 
-    var response = await fetch(" http://localhost:3001/wjtop")
-    return await response.json()
-}
-
-
-console.log(GetLJ());
-console.log(GetLJB());
-console.log(Getbhop());
-console.log(GetMbhop());
-console.log(GetDbop());
-console.log(GetWJ());
-
-
-
-export default function Home(){       
-    let [data, setData] = useState({
-        "name": "loading...",
-        "connect": "",
-        "map": "",
-        "playercount": ""
+let LJ = () =>{
+    let [LJData, setLJData] = useState({  "":"" // dont forget to edit this 
     });
     useEffect( () => {
-        fetch("http://localhost:3001/ServerSts").then(res => res.json()).then(dt => setData({
-            "name": dt.name,
-            "connect": "steam://connect/"+dt.connect,
-            "map": dt.map,
-            "playercount": dt.raw.numplayers
+        fetch("http://localhost:3001/ljtop").then(res => res.json()).then(dt => setLJData({ dt // and this
         }))
     }
     )
+}
+
+let Bhop = () =>{
+    let [LJData, setLJData] = useState({  "":"" // dont forget to edit this
+    });
+    useEffect( () => {
+        fetch("http://localhost:3001/bhoptop").then(res => res.json()).then(dt => setLJData({ dt // and this
+        }))
+    }
+    )
+}
+
+let BlockLJ = () =>{
+    let [LJData, setLJData] = useState({  "":"" // dont forget to edit this
+    });
+    useEffect( () => {
+        fetch("http://localhost:3001/ljblocktop").then(res => res.json()).then(dt => setLJData({ dt // and this
+        }))
+    }
+    )
+}
+
+let DropBhop = () =>{
+    let [LJData, setLJData] = useState({  "":"" // dont forget to edit this
+    });
+    useEffect( () => {
+        fetch("http://localhost:3001/dropbhoptop").then(res => res.json()).then(dt => setLJData({ dt // and this
+        }))
+    }
+    )
+}
+
+let LJ = () =>{
+    let [LJData, setLJData] = useState({ "":"" // dont forget to edit this
+    });
+    useEffect( () => {
+        fetch("http://localhost:3001/multibhoptop").then(res => res.json()).then(dt => setLJData({ dt // and this
+        }))
+    }
+    )
+}
+
+let Bhop = () =>{
+    let [LJData, setLJData] = useState({  "":"" // dont forget to edit this
+    });
+    useEffect( () => {
+        fetch("http://localhost:3001/wjtop").then(res => res.json()).then(dt => setLJData({ dt // and this
+        }))
+    }
+    )
+}
+
+export default function Home(){
     return(
         <>
             <div className = "Wraper">
@@ -68,7 +73,7 @@ export default function Home(){
                 <Link to="/Leaderboard/mbhop" className="navel" title="Demos"><h1>MBhop</h1></Link>
                 <Link to="/Leaderboard/dbhop" className="navel" title="Demos"><h1>DBhop</h1></Link>
             </div>
-            <Route exact path="/Leaderboard/lj"></Route>
+            <Route exact path="/Leaderboard/lj"><LJ /></Route>
             <Route path="/Leaderboard/bhop"></Route>
             <Route path="/Leaderboard/blocklj"></Route>
             <Route path="/Leaderboard/mbhop"></Route>
